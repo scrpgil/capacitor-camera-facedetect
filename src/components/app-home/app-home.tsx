b00ab3318f2d7dda76b8fed366fd7e651637261c
scrpgil <scraplbuild@gmail.com>
2018-11-30 03:47:18 +0900

    add clmtracker

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,5 +1,6 @@
-import { Component, Prop, State } from "@stencil/core";
+import { Component, Element, Prop, State } from "@stencil/core";
 import { Plugins, CameraResultType } from "@capacitor/core";
+import * as Tracker from "clmtrackr";
 
 const { Camera } = Plugins;
 
@@ -11,6 +12,14 @@ export class AppHome {
   @State() src: string = "";
   @Prop({ connect: "ion-pwa-camera-modal" })
   cameraModal: HTMLIonPwaCameraModalElement;
+  @Element() el: HTMLElement;
+
+  ctrack: any;
+
+  async componentWillLoad() {
+    console.log(Tracker.default);
+    this.ctrack = new Tracker.default.tracker({ useWebGL: false });
+  }
 
   async takePicture() {
     const image = await Camera.getPhoto({
@@ -18,8 +27,48 @@ export class AppHome {
       allowEditing: true,
       resultType: CameraResultType.Uri
     });
+    this.resetCanvas();
+
     var imageUrl = image.webPath;
     this.src = imageUrl;
+
+    let img = new Image();
+    img.onload = () => {
+      const imageCanvas: any = this.el.querySelector("#image");
+      const cc = imageCanvas.getContext("2d");
+      cc.drawImage(img, 0, 0, img.width, img.height);
+      this.drawAnimation();
+    };
+    img.src = this.src;
+  }
+
+  resetCanvas() {
+    this.ctrack.reset();
+    const imageCanvas: any = this.el.querySelector("#image");
+    const cc = imageCanvas.getContext("2d");
+    cc.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+
+    const overlay: any = this.el.querySelector("#overlay");
+    const overlayCC = overlay.getContext("2d");
+    overlayCC.clearRect(0, 0, overlay.width, overlay.height);
+  }
+
+  drawAnimation() {
+    this.ctrack.init();
+    this.ctrack.start(this.el.querySelector("#image"));
+    const overlay: any = this.el.querySelector("#overlay");
+    const overlayCC = overlay.getContext("2d");
+    let cnt = 0;
+    let id = setInterval(() => {
+      overlayCC.clearRect(0, 0, overlay.width, overlay.height);
+      if (this.ctrack.getCurrentPosition()) {
+        this.ctrack.draw(overlay);
+      }
+      cnt = cnt + 1;
+      if (cnt >= 20) {
+        clearInterval(id);
+      }
+    }, 80);
   }
 
   render() {
@@ -31,7 +80,10 @@ export class AppHome {
       </ion-header>,
 
       <ion-content padding>
-        <img src={this.src} />
+        <div id="container">
+          <canvas id="image" width="625" height="500" />
+          <canvas id="overlay" width="625" height="500" />
+        </div>
         <ion-button onClick={() => this.takePicture()}>Camera</ion-button>
       </ion-content>
     ];
